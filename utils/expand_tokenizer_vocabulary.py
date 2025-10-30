cdd1f2c721b75516cbcfb65daae75b1a4d97bfa8
趙健宏 <chaochienhungludwig123@yahoo.com.tw>
2025-10-31 01:57:20 +0800

    Add expand_tokenizer utils file

--- utils/expand_tokenizer_vocabulary.py
+++ utils/expand_tokenizer_vocabulary.py
@@ -1,44 +1,22 @@
 import os
-import numpy as np
-import pandas as pd
-from transformers import AutoTokenizer
+from config import TOKENIZER, TOKENIZER_CACHE, MODEL, MODEL_CACHE
+from transformers import AutoTokenizer, AutoModel
 
+tokenizer = AutoTokenizer.from_pretrained(TOKENIZER, cache_dir=os.path.join(TOKENIZER_CACHE, "toxic"))
+model = AutoModel.from_pretrained(MODEL, cache_dir=os.path.join(MODEL_CACHE, "toxic"))
 
-# Create the directory if it doesn't exist
-save_path = "../models/tokenizer/vocab_expanded_tokenizer"
-os.makedirs(save_path, exist_ok=True)
+# new tokens
+new_tokens = ["[ar]", "[bg]", "[de]", "[el]", "[en]", "[es]", 
+              "[fr]", "[hi]", "[it]", "[ja]", "[nl]", "[pl]", "[pt]",
+              "[ru]", "[sw]", "[th]", "[tr]", "[ur]", "[vi]", "[zh]"]
 
-def get_training_corpus(file_paths: list[str]):
-    dataset = pd.Series([])
-    for file_path in file_paths:
-        df = pd.read_csv(file_path)
-        if "text" in df.keys():
-            dataset = pd.concat([dataset, df["text"]])
+existing_tokens = set(tokenizer.get_vocab().keys())
+new_tokens = set(new_tokens) - existing_tokens
 
-        else:
-            dataset = pd.concat([dataset, df['messageContent']])
-    
-    for start_idx in range(0, len(dataset), 1000):
-        samples = dataset[start_idx: start_idx + 1000]
-        yield samples
+# add the tokens to the tokenizer vocabulary
+tokenizer.add_tokens(list(new_tokens))
 
-# Initialize the tokenizer
-tokenizer_path = "../models/huggingface/tokenizer"
-tokenizer = AutoTokenizer.from_pretrained("unitary/toxic-bert", cache_dir=tokenizer_path)
-
-# Train on additional corpus
-training_corpus = get_training_corpus(file_paths)
-extra_corpus_tokenizer = tokenizer.train_new_from_iterator(training_corpus, 45000)
-
-emoji = np.genfromtxt('../dataset/emoji.txt', dtype=str, delimiter='\n', encoding='utf-8')
-
-# Add new tokens to the existing tokenizer vocabulary
-new_vocabulary = list(extra_corpus_tokenizer.vocab.keys())
-new_vocabulary = set(new_vocabulary) | set(emoji) - set(tokenizer.vocab.keys())
-
-tokenizer.add_tokens(list(new_vocabulary))
-
-# Save the tokenizer with the expanded vocabulary
-tokenizer.save_pretrained(save_path)
-
-print(f"Tokenizer saved at {save_path}")
+# add new, random embeddings for the new tokens
+model.resize_token_embeddings(len(tokenizer))
+model.save_pretrained(os.path.join(MODEL_CACHE, "toxic"))
+tokenizer.save_pretrained(os.path.join(TOKENIZER_CACHE, "toxic"))
